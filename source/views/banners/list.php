e4bf42d7e0139ba68c7eeba7332fcea8ce7c854f
Willian Stori <programacao2@lovatel.com.br>
2022-10-21 16:41:34 -0300

    Ajustes de estrutura

--- source/views/banners/list.php
+++ source/views/banners/list.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listar Banners</title>
 </head>
+
 <body>
     <h1>Lista de Banners</h1>
+    
     <ul>
-        <?php foreach($banners as $banner){?>
-            <li><?= $banner->getMessage()?></li>
-        <?php }?>
+        <?php foreach ($banners as $banner) { ?>
+            <li><?= $banner->getMessage() ?></li>
+        <?php } ?>
     </ul>
 </body>
+
 </html>
