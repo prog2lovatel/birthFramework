94f74c0dbed8ef51167d442895a188cd5c14cb5c
Willian Stori <programacao2@lovatel.com.br>
2022-10-21 16:40:45 -0300

    Ajuste de Titles

--- source/views/banners/edit.php
+++ source/views/banners/edit.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Banner Criar</title>
+    <title>Alterar Banner</title>
 </head>
 <body>
     <h1>Alteração de Banner</h1>
--- source/views/banners/list.php
+++ source/views/banners/list.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Banners</title>
+    <title>Listar Banners</title>
 </head>
 <body>
-    <h1>Lista</h1>
+    <h1>Lista de Banners</h1>
     <ul>
         <?php foreach($banners as $banner){?>
             <li><?= $banner->getMessage()?></li>
